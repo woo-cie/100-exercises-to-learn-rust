62beafca32d034c5cd3e0a252ce1e88c8d2d58af
Makoto Yamada <woocie.rugby@gmail.com>
2024-08-18 09:21:05 +0900

    04-12

--- exercises/04_traits/12_copy/src/lib.rs
+++ exercises/04_traits/12_copy/src/lib.rs
@@ -1,6 +1,7 @@
 // TODO: implement the necessary traits to make the test compile and pass.
 //  You *can't* modify the test.
 
+#[derive(Debug, Clone, Copy, std::cmp::PartialEq)]
 pub struct WrappingU32 {
     value: u32,
 }
@@ -8,6 +9,13 @@ pub struct WrappingU32 {
 impl WrappingU32 {
     pub fn new(value: u32) -> Self {
         Self { value }
+    }
+}
+
+impl std::ops::Add for WrappingU32 {
+    type Output = WrappingU32;
+    fn add(self, rhs: Self) -> Self::Output {
+        Self::new(self.value.wrapping_add(rhs.value))
     }
 }
 
