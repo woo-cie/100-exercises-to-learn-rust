ab3c02aa73330acf0716dca9e9c9a1d743dd6cb2
Makoto Yamada <woocie.rugby@gmail.com>
2024-08-19 23:55:16 +0900

    06-11

--- exercises/06_ticket_management/11_mutable_slices/src/lib.rs
+++ exercises/06_ticket_management/11_mutable_slices/src/lib.rs
@@ -1,6 +1,12 @@
 // TODO: Define a function named `squared` that raises all `i32`s within a slice to the power of 2.
 //  The slice should be modified in place.
 
+fn squared(v: &mut [u32]) -> () {
+    for mut i in v {
+        *i *= *i;
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
