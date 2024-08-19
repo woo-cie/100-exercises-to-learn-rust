1fcdfd24f6818505c6b38a9959c88e8b22570350
Makoto Yamada <woocie.rugby@gmail.com>
2024-08-19 23:49:29 +0900

    06-10

--- exercises/06_ticket_management/10_slices/src/lib.rs
+++ exercises/06_ticket_management/10_slices/src/lib.rs
@@ -1,6 +1,14 @@
 // TODO: Define a function named `sum` that takes a reference to a slice of `u32` and returns the sum of all
 //  elements in the slice.
 
+fn sum(v: &[u32]) -> u32 {
+    let mut sum = 0u32;
+    for i in v {
+        sum += i;
+    }
+    sum
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
