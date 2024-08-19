f80b5882661f809d15ed3e716d0f194d4ff27d12
Makoto Yamada <woocie.rugby@gmail.com>
2024-08-19 22:27:25 +0900

    06-02

--- exercises/06_ticket_management/02_vec/src/lib.rs
+++ exercises/06_ticket_management/02_vec/src/lib.rs
@@ -15,7 +15,12 @@ pub fn fibonacci(n: u32) -> u32 {
     //
     // Hint: use a `Vec` to memoize the results you have already calculated
     // so that you don't have to recalculate them several times.
-    todo!()
+    let mut memo: Vec<u32> = vec![0, 1];
+    let n = n as usize;
+    for i in 2..=n {
+        memo.push(memo[i - 1] + memo[i - 2]);
+    }
+    memo[n]
 }
 
 #[cfg(test)]
