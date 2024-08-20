8b78d122ac3d18aec7e19981d30909e6b98d8a6f
Makoto Yamada <woocie.rugby@gmail.com>
2024-08-20 10:23:11 +0900

    07-02

--- exercises/07_threads/02_static/src/lib.rs
+++ exercises/07_threads/02_static/src/lib.rs
@@ -4,7 +4,13 @@
 use std::thread;
 
 pub fn sum(slice: &'static [i32]) -> i32 {
-    todo!()
+    let mid = slice.len() / 2;
+    let (v1, v2) = slice.split_at(mid);
+
+    let handle1 = thread::spawn(move || v1.iter().sum::<i32>());
+    let handle2 = thread::spawn(move || v2.iter().sum::<i32>());
+
+    handle1.join().unwrap() + handle2.join().unwrap()
 }
 
 #[cfg(test)]
